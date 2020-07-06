9ac67ef54012d0521468ae0352f822d41c602101
michellanet <michellanet@gmail.com>
2020-07-06 11:12:04 -0400

    update

--- smartresume-master/frontend/src/components/pages/JobObjective.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function JobObjective() {
-  return (
-    <div>Job Objective</div>
-  );
-}
-
-export default JobObjective;
--- smartresume-master/frontend/src/components/pages/Skill.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Skill() {
-  return (
-    <div>Skill</div>
-  );
-}
-
-export default Skill;
