58fcb6530665b1ef8c0eefa2c66a99b1f3924e75
michellanet <michellanet@gmail.com>
2020-07-06 11:42:41 -0400

    update

---
+++ frontend/src/components/pages/AddCoverLetter.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Form, Container, Jumbotron, Accordion, Card, Button} from 'react-bootstrap';
+
+function CoverLetter() {
+  return (
+    <Container>
+    <Jumbotron>
+    <Form className="mt-4 mb-4">
+  <Form.Group controlId="title">
+    <Form.Control type="text" placeholder="Title" />
+  </Form.Group>
+  <Form.Group controlId="letter">
+    <Form.Control as="textarea" rows="6" placeholder="Body.." />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Save
+  </Button>
+</Form>
+    </Jumbotron>
+</Container>
+  );
+}
+
+export default CoverLetter;
