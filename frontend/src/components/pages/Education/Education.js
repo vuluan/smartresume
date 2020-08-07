0e9f69b7ea9274861c563224bed54c253e44deb0
Roman <romanmbwasi@gmail.com>
2020-08-07 19:59:58 -0400

    Changed education to fetch data from API

--- frontend/src/components/pages/Education/Education.js
+++ frontend/src/components/pages/Education/Education.js
@@ -1,37 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import EducationForm from './EducationForm';
 import EducationTable from './EducationTable';
 import Breadcrumbs from '../../layouts/Breadcrumbs';
-
+import axios from 'axios';
 
 function Education() {
+  let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImVtYWlsIjoidGVzdGFwcDU1QG1id2FzaS5jb20iLCJfaWQiOiI1ZjJjZDM3NjJlZTlkYjAwMTcyOGVkOTIifSwiaWF0IjoxNTk2NzczNDg5fQ.MHKYgOw8wmHIVn7SM2qNliVQUPfJoWJV5FImkHmWZeI'
 
-  const [education, setEducation] = useState([
-    {
-      id: 1,
-      school: "Humber College",
-      degree: "Post Grad Diploma",
-      field: "IT",
-      start: 2019,
-      finish: 2020
-    },
-    {
-      id: 2,
-      school: "UCT",
-      degree: "Bachelors of Commerce",
-      field: "Information Systems",
-      start: 2019,
-      finish: 2020
-    },
-    {
-      id: 3,
-      school: "Agha Khan Mzizima",
-      degree: "Diploma",
-      field: "PCM",
-      start: 2019,
-      finish: 2020
-    },
-  ]);
+  const [education, setEducation] = useState([]);
+
+  const getEducationList = () => {
+    axios.get('https://smartresumebuild.herokuapp.com/api/education/list/5f2cd3762ee9db001728ed92', {
+      headers: {
+        Authorization: 'Bearer ' + token //the token is a variable which holds the token
+      }
+    }).then((response) => {
+      console.log(response.data.data);
+      setEducation(response.data.data);
+    });
+  }
+
+  useEffect(() => {
+    getEducationList();
+  }, [])
 
   const handleEdit = () => {
     console.log("Edit clicked");
