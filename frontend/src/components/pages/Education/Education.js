14fe906eeb31f868f19ac633168a9e6d3c606f6a
Roman <romanmbwasi@gmail.com>
2020-08-08 23:42:20 -0400

    Education done

--- frontend/src/components/pages/Education/Education.js
+++ frontend/src/components/pages/Education/Education.js
@@ -3,10 +3,11 @@ import { Modal, Button } from 'react-bootstrap';
 import EducationForm from './EducationForm';
 import EducationTable from './EducationTable';
 import Breadcrumbs from '../../layouts/Breadcrumbs';
-import axios from 'axios';
+import axios, { HOST } from '../../../utils/httpUtilities';
+import LocalStorageService from './../../../utils/localStorage';
 
 function Education() {
-  let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImVtYWlsIjoidGVzdGFwcDU1QG1id2FzaS5jb20iLCJfaWQiOiI1ZjJjZDM3NjJlZTlkYjAwMTcyOGVkOTIifSwiaWF0IjoxNTk2NzczNDg5fQ.MHKYgOw8wmHIVn7SM2qNliVQUPfJoWJV5FImkHmWZeI'
+  let BASE_URL = 'https://smartresumebuild.herokuapp.com/api';
 
   const [education, setEducation] = useState([]);
   const [formValues, setFormValues] = useState({});
@@ -16,127 +17,61 @@ function Education() {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  const getEducationList = () => {
-    axios.get('https://smartresumebuild.herokuapp.com/api/education/list/5f2cd3762ee9db001728ed92', {
-      headers: {
-        Authorization: 'Bearer ' + token //the token is a variable which holds the token
-      }
-    }).then((response) => {
-      console.log(response.data.data);
-      setEducation(response.data.data);
-    });
-  }
-
-  const deleteEducationById = (index) => {
-    console.log(education[index]);
-    // axios.delete('https://smartresumebuild.herokuapp.com/api/education', {
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //     Authorization: 'Bearer ' + token //the token is a variable which holds the token
-    //   }
-    // }).then((response) => {
-    //   console.log(response.data.data);
-    //   setEducation(response.data.data);
-    // });
-  }
-
   useEffect(() => {
     getEducationList();
   }, [])
 
-  const handleEdit = (index) => {
-    console.log(`Index: ${index}`);
-    console.log(`Edit clicked: ${index}`);
-    setFormValues(education[index]);
-    setIsEditing(true);
-    setShow(currentSet => !currentSet);
-  }
-
-  const handleAddNew = () => {
-    console.log(`Add New`);
-    setFormValues({})
-    setIsEditing(false);
-    setShow(currentSet => !currentSet);
+  const getEducationList = () => {
+    let userId = LocalStorageService.getUserInfo().userId;
+    axios.get(`${BASE_URL}/education/list/${userId}`).then((response) => {
+      setEducation(response.data.data);
+    });
   }
 
   const handleSave = (data) => {
-    console.log("Save clicked: " + JSON.stringify(data));
-    //data.id = education.length + 1;
-
     if (isEditing) {
-      data.user_id = '5f2cd3762ee9db001728ed92'
+      data.user_id = LocalStorageService.getUserInfo().userId;
       data.id = formValues._id
-      axios.put('https://smartresumebuild.herokuapp.com/api/education', data, {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer ' + token //the token is a variable which holds the token
-        }
-      }).then((response) => {
-        console.log(response.data.data);
+      axios.put(`${BASE_URL}/education`, data).then((response) => {
         setShow(false)
         getEducationList();
       }).catch((error) => {
         console.log(error);
       });
     } else {
-      data.user_id = '5f2cd3762ee9db001728ed92'
-      axios.post('https://smartresumebuild.herokuapp.com/api/education/add', data, {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer ' + token //the token is a variable which holds the token
-        }
-      }).then((response) => {
-        console.log(response.data.data);
-        setShow(false)
-        getEducationList();
-      }).catch((error) => {
-        console.log(error);
-      });
+      data.user_id = LocalStorageService.getUserInfo().userId;
+      axios.post(`${BASE_URL}/education/add`, data)
+        .then((response) => {
+          setShow(false)
+          getEducationList();
+        }).catch((error) => {
+          console.log(error);
+        });
 
     }
-
-
-
   }
-
-  const handleSaveEdit = (data) => {
-    console.log("Save clicked: " + JSON.stringify(data));
-    //data.id = education.length + 1;
-    data.user_id = '5f2cd3762ee9db001728ed92'
-    axios.post('https://smartresumebuild.herokuapp.com/api/education/add', data, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + token //the token is a variable which holds the token
-      }
-    }).then((response) => {
-      console.log(response.data.data);
-      setShow(false)
-      getEducationList();
-
-    }).catch((error) => {
-      console.log(error);
-    });
-  }
-
-
 
   const handleDelete = (id) => {
-    console.log("Delete clicked:" + JSON.stringify({ id }));
     let deleteId = { id }
-
-    axios.delete('https://smartresumebuild.herokuapp.com/api/education', {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + token //the token is a variable which holds the token
-      },
+    axios.delete(`${BASE_URL}/education`, {
       data: deleteId
     }).then((response) => {
-      console.log(response.data);
       getEducationList();
     }).catch((error) => {
       console.log(error);
     });
-    // setEducation(prevEducation => prevEducation.filter((g) => g.id !== id))
+  }
+
+  const handleEdit = (index) => {
+    setFormValues(education[index]);
+    setIsEditing(true);
+    setShow(currentSet => !currentSet);
+  }
+
+  const handleAddNew = () => {
+    setFormValues({})
+    setIsEditing(false);
+    setShow(currentSet => !currentSet);
   }
 
   const breadcrumbLinks = [
@@ -153,7 +88,6 @@ function Education() {
 
   return (
     <>
-
       <Breadcrumbs links={breadcrumbLinks} />
       <Modal
         show={show}
@@ -171,7 +105,6 @@ function Education() {
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-
         </Modal.Footer>
       </Modal>
 
